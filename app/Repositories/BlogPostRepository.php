880231c8af239318c379a638e8ba302311ec5b55
Dmytro <dmytro.dolzhenko@oa.edu.ua>
2025-06-16 07:28:57 +0000

    Лабораторна 7. Вивід статей блога

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Database\Eloquent\Collection; // Цей use може бути не використаний прямо, але нехай буде для прикладу.
+
+/**
+ * Class BlogPostRepository.
+ */
+class BlogPostRepository extends CoreRepository
+{
+    protected function getModelClass()
+    {
+        return Model::class; // Абстрагування моделі BlogPost, для легшого створення іншого репозиторія
+    }
+
+    /**
+     * Отримати список статей з пагінацією
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAllWithPaginate()
+    {
+        // Вказуємо тільки необхідні колонки для оптимізації запиту
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id',
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC') // Сортуємо за ID в зворотньому порядку (новіші перші)
+            ->paginate(25); // Встановлюємо 25 записів на сторінку
+
+        return $result;
+    }
+
+    /**
+     * Отримати модель для редагування в адмінці
+     * @param int $id
+     * @return Model
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+}
