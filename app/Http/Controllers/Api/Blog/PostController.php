e132779c6b65c51184e5f4083f8a5c87bc624ab1
Dmytro <dmytro.dolzhenko@oa.edu.ua>
2025-06-20 11:28:49 +0000

    Лабораторна 15. Vue, API

---
+++ app/Http/Controllers/Api/Blog/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog;
+
+use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 1000);
+
+        $posts = BlogPost::with(['user', 'category'])->paginate($perPage);
+
+        return response()->json($posts);
+    }
+}
